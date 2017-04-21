622cbd6b67f87cee59e9b456bbdba848774cef50
yubing <29988076@qq.com>
2017-04-21 11:51:55 +0800

    增加权限

--- edu_client_backstage/src/views/compontents/common/left.vue
+++ edu_client_backstage/src/views/compontents/common/left.vue
@@ -1,19 +1,19 @@
 <template>
 	<div class="left">
 		<el-menu :default-active="activeMenu"  :router=true class="" theme="dark">
-			<el-menu-item index="globalControl"><i class="nav-icon nav-icon1"></i>全局管理</el-menu-item>
-			<el-submenu index="memberManage">
+			<el-menu-item index="globalControl" ><i class="nav-icon nav-icon1"></i>全局管理</el-menu-item>
+			<el-submenu index="memberManage" v-if="numlist.indexOf(23) != -1">
 				<template slot="title"><i class="nav-icon nav-icon2"></i>会员管理</template>
 				<el-menu-item-group>
 					<el-menu-item index="memberManage">会员列表</el-menu-item>
-					<el-menu-item index="memberCheck">审核</el-menu-item>
+					<el-menu-item index="memberCheck" v-if="numlist.indexOf(25)!=-1">审核</el-menu-item>
 					<!--<el-menu-item index="memberAdd">添加会员</el-menu-item>-->
-				</el-menu-item-group>
+				</el-menu-item-group> 
 			</el-submenu>
-			<el-menu-item index="administrator"><i class="nav-icon nav-icon3"></i>管理员</el-menu-item>
+			<el-menu-item index="administrator" v-if="numlist.indexOf(7) != -1"><i class="nav-icon nav-icon3" ></i>管理员</el-menu-item>
 			<el-menu-item index="systemMessage"><i class="nav-icon nav-icon4"></i>系统通知</el-menu-item>
-			<el-menu-item index="packageManage"><i class="nav-icon nav-icon5"></i>套餐管理</el-menu-item>
-			<el-menu-item index="systemSet"><i class="nav-icon nav-icon6"></i>系统设置</el-menu-item>
+			<el-menu-item index="packageManage" v-if="numlist.indexOf(11) != -1"><i class="nav-icon nav-icon5" ></i>套餐管理</el-menu-item>
+			<el-menu-item index="systemSet" v-if="numlist.indexOf(8) != -1"><i class="nav-icon nav-icon6" ></i>系统设置</el-menu-item>
 		</el-menu>
 	</div>
 </template>
@@ -23,10 +23,13 @@
 	     data(){
 	        return{
                 activeMenu: 'globalControl',
+                numlist:"",
 			}
 		 },
 		 methods: {
-
+		 	onloadLimits(params){
+		 		this.numlist = params;
+		 	},
 		 },
 		 watch: {
 			 /*
@@ -37,7 +40,6 @@
                  handler(val) {
                      if(val){
                          let url=val.split('/')[2],nowNav=null;
-
                          if(url.indexOf("globalControl")!= -1) nowNav="globalControl";
                          if(url.indexOf("memberManage")!= -1) nowNav="memberManage";
                          if(url.indexOf("memberCheck") != -1) nowNav="memberCheck";
